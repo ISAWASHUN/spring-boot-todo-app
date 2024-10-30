e0d39a16fbf8f77119ce6fb8ca92136d299083c7
ISAWASHUN <syun20000616@icloud.com>
2024-10-30 23:03:10 +0900

    SQLの作成

---
+++ sql/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO todos (todo, detail, created_at, updated_at) VALUES ('Learn SQL', 'Learn SQL from scratch', '2021-01-01 00:00:00', '2021-01-01 00:00:00');
+INSERT INTO todos (todo, detail, created_at, updated_at) VALUES ('Learn Go', 'Learn Go from scratch', '2021-01-01 00:00:00', '2021-01-01 00:00:00');
+INSERT INTO todos (todo, detail, created_at, updated_at) VALUES ('Learn Docker', 'Learn Docker from scratch', '2021-01-01 00:00:00', '2021-01-01 00:00:00');
